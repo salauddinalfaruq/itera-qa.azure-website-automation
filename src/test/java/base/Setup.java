716dfbccf40262e9cacfdf1511ee39b9bbc5e464
salauddinalfaruq <salahuddinalfaruq@gmail.com>
2023-05-22 21:43:14 +0600

    initial commit

---
+++ src/test/java/base/Setup.java
@@ -0,0 +1,63 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import utils.Utils;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class Setup {
+    public WebDriver driver;
+
+    @BeforeTest
+    public void Setup(){
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @AfterMethod
+    public void screenShot(ITestResult result) throws IOException {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            try {
+                Utils utils = new Utils();
+                utils.takeScreenshot(driver);
+            } catch (Exception exception) {
+                System.out.println(exception.toString());
+            }
+        }
+    }
+
+/*  @AfterMethod
+    public void screenShot(ITestResult result) throws IOException {
+        if (ITestResult.SUCCESS == result.getStatus()){
+            try {
+                Utils utils = new Utils();
+                utils.takeScreenshot(driver);
+            }
+            catch (Exception exception){
+                System.out.println(exception.toString());
+            }
+        }
+        else if  (ITestResult.FAILURE == result.getStatus()){
+            try {
+                Utils utils = new Utils();
+                utils.takeScreenshot(driver);
+            }
+            catch (Exception exception){
+                System.out.println(exception.toString());
+            }
+        }
+    } */
+
+    // @AfterTest
+    public void closeDriver()
+    {
+        driver.close();
+    }
+}
+
